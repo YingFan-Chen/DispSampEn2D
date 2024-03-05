df6683e1e8385c03f4b524fa2960ad152f85fc67
YingFan-Chen <b07502024@ntu.edu.tw>
2024-03-05 19:41:43 +0800

    add multiple test case

--- unittest.py
+++ unittest.py
@@ -5,6 +5,7 @@ from lib.entropy.DispSampEn2D import *
 from lib.entropy.SampEn2D import *
 from timeit import default_timer as timer
 
+# Test case1
 img = load_image("./image/target/kylberg_0.0/blanket1_0.jpg")
 
 start = timer()
@@ -21,3 +22,21 @@ start = timer()
 result = DispEn2D(img)
 print("DispEn2D run time:", timer() - start)
 assert abs(result - 4.7480125) < 0.000001
+
+# Test case2
+img = load_image("./image/target/kylberg_0.0/blanket1_1.jpg")
+
+start = timer()
+result = SampEn2D(img)
+print("SampEn2D run time:", timer() - start)
+assert abs(result - 5.9833763) < 0.000001
+
+start = timer()
+result = DispSampEn2D(img)
+print("DispSampEn2D run time:", timer() - start)
+assert abs(result - 0.7574938) < 0.000001
+
+start = timer()
+result = DispEn2D(img)
+print("DispEn2D run time:", timer() - start)
+assert abs(result - 4.7017913) < 0.000001
